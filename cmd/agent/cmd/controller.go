fce8c8db19f6c51ff60b298b2b0ddd313f6659da
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2024-09-23 17:30:32 +0800

    ebpf
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

---
+++ cmd/agent/cmd/controller.go
@@ -0,0 +1,68 @@
+// Copyright 2022 Authors of spidernet-io
+// SPDX-License-Identifier: Apache-2.0
+
+package cmd
+
+import (
+	"context"
+	"go.uber.org/zap"
+	corev1 "k8s.io/api/core/v1"
+	v1beta1 "k8s.io/api/discovery/v1beta1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+type reconciler struct {
+	// client can be used to retrieve objects from the APIServer.
+	client client.Client
+	log    zap.Logger
+}
+
+func (r *reconciler) Reconcile(ctx context.Context, req reconcile.Request) (reconcile.Result, error) {
+	r.log.Sugar().Infof("Reconcile: %v", req)
+}
+
+func SetupController() {
+	logger := rootLogger.Named("controller")
+
+	mgr, err := manager.New(config.GetConfigOrDie(), manager.Options{
+		Metrics: metricsserver.Options{BindAddress: "0"},
+	})
+	if err != nil {
+		logger.Sugar().Fatalf(err, "unable to set up controller: %v ", err)
+	}
+
+	r := reconciler{
+		client: mgr.GetClient(),
+		log:    logger,
+	}
+	// Setup a new controller to reconcile ReplicaSets
+	logger.Sugar().Info("Setting up controller")
+	c, err := controller.New("agent", mgr, controller.Options{
+		Reconciler: &r,
+	})
+	if err != nil {
+		logger.Sugar().Fatalf(err, "unable to set up individual controller: %v", err)
+	}
+
+	// Watch ReplicaSets and enqueue ReplicaSet object key
+	if err := c.Watch(source.Kind(mgr.GetCache(), &corev1.Service{}, &handler.TypedEnqueueRequestForObject[*corev1.Service]{})); err != nil {
+		logger.Sugar().Fatalf(err, "unable to watch service: %v", err)
+	}
+	if err := c.Watch(source.Kind(mgr.GetCache(), &v1beta1.EndpointSlice{}, &handler.TypedEnqueueRequestForObject[*v1beta1.EndpointSlice]{})); err != nil {
+		logger.Sugar().Fatalf(err, "unable to watch EndpointSlice: %v", err)
+	}
+
+	logger.Sugar().Info("starting manager")
+	if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
+		logger.Sugar().Fatalf(err, "unable to run manager: %v", err)
+	}
+
+}
--- cmd/agent/cmd/daemon.go
+++ cmd/agent/cmd/daemon.go
@@ -4,13 +4,8 @@
 package cmd
 
 import (
-	"context"
-	"github.com/spidernet-io/rocktemplate/api/v1/grpcService"
 	"github.com/spidernet-io/rocktemplate/pkg/debug"
-	"github.com/spidernet-io/rocktemplate/pkg/grpcManager"
 	"github.com/spidernet-io/rocktemplate/pkg/types"
-	"github.com/spidernet-io/rocktemplate/pkg/utils"
-	"time"
 )
 
 func SetupUtility() {
@@ -25,53 +20,15 @@ func SetupUtility() {
 	}
 }
 
-func testGrpc() {
-	// ---- grpc server
-	rootLogger.Info("start grpc server")
-	alternateDNS := []string{}
-	// add pod name
-	alternateDNS = append(alternateDNS, types.AgentConfig.PodName)
-	tlsCertPath := "/tmp/cert.crt"
-	tlsKeyPath := "/tmp/key.crt"
-	tlsCaPath := "/tmp/ca.crt"
-	// generate self-signed certificates
-	if e := utils.NewServerCertKeyForLocalNode(alternateDNS, tlsCertPath, tlsKeyPath, tlsCaPath); e != nil {
-		rootLogger.Sugar().Fatalf("failed to generate certiface, error=%v", e)
-	}
-	t := grpcManager.NewGrpcServer(rootLogger, tlsCertPath, tlsKeyPath)
-	// listen on ipv4 and ipv6
-	t.Run(":3000")
-	time.Sleep(time.Second * 10)
-
-	// self test grpc
-	rootLogger.Info("test grpc server")
-	client := grpcManager.NewGrpcClient(rootLogger, true)
-	p := &grpcService.ExecRequestMsg{
-		Command:       "ls /",
-		Timeoutsecond: 10,
-	}
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	if reslist, err := client.SendRequestForExecRequest(ctx, []string{"127.0.0.1:3000"}, []*grpcService.ExecRequestMsg{p}); err != nil {
-		rootLogger.Sugar().Errorf("grpc failed to send request, error=%v", err)
-	} else {
-		rootLogger.Sugar().Infof("grpc response: %+v", reslist[0])
-	}
-	cancel()
-
-}
-
 func DaemonMain() {
 	rootLogger.Sugar().Infof("config: %+v", types.AgentConfig)
 
 	SetupUtility()
 
-	SetupHttpServer()
+	// SetupHttpServer()
 
-	RunMetricsServer(types.AgentConfig.PodName)
+	// RunMetricsServer(types.AgentConfig.PodName)
 
-	testGrpc()
+	SetupController()
 
-	// -----------
-	rootLogger.Info("hello world")
-	time.Sleep(time.Hour)
 }
