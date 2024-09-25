b47f343d5c748951f9800541a8e72532999cef76
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2024-09-25 16:21:04 +0800

    d
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- cmd/inspect/cmd/cleanMapAll.go
+++ cmd/inspect/cmd/cleanMapAll.go
@@ -1,1 +1,61 @@
 package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"github.com/spidernet-io/rocktemplate/pkg/ebpf"
+	"os"
+)
+
+var CmdCleanMapAll = &cobra.Command{
+	Use:   "affinity",
+	Short: "clean all ebpf map ",
+	Args:  cobra.RangeArgs(0, 0),
+	Run: func(cmd *cobra.Command, args []string) {
+		bpf := ebpf.NewEbpfProgramMananger(nil)
+		if err := bpf.LoadAllEbpfMap(""); err != nil {
+			fmt.Printf("failed to load ebpf Map: %v\n", err)
+			os.Exit(2)
+		}
+		defer bpf.UnloadAllEbpfMap()
+
+		fmt.Printf("\n")
+		fmt.Printf("clean all ebpf map:\n")
+		if c, e := bpf.CleanMapService(); e != nil {
+			fmt.Printf("    failed to clean service map: %+v\n", e)
+			os.Exit(3)
+		} else {
+			fmt.Printf("    succeeded to clean %d items in service map \n", c)
+		}
+		if c, e := bpf.CleanMapBackend(); e != nil {
+			fmt.Printf("    failed to clean backend map: %+v\n", e)
+			os.Exit(3)
+		} else {
+			fmt.Printf("    succeeded to clean %d items in backend map\n", c)
+		}
+		if c, e := bpf.CleanMapNode(); e != nil {
+			fmt.Printf("    failed to clean node map: %+v\n", e)
+			os.Exit(3)
+		} else {
+			fmt.Printf("    succeeded to clean %d items in node map\n", c)
+		}
+		if c, e := bpf.CleanMapNatRecord(); e != nil {
+			fmt.Printf("    failed to clean natRecord map: %+v\n", e)
+			os.Exit(3)
+		} else {
+			fmt.Printf("    succeeded to clean %d items in natRecord map\n", c)
+		}
+		if c, e := bpf.CleanMapAffinity(); e != nil {
+			fmt.Printf("    failed to clean affinity map: %+v\n", e)
+			os.Exit(3)
+		} else {
+			fmt.Printf("    succeeded to clean %d items in affinity map\n", c)
+		}
+
+		fmt.Printf("\n")
+	},
+}
+
+func init() {
+	CmdCleanMap.AddCommand(CmdCleanMapAll)
+}
--- cmd/inspect/cmd/cleanMapNatRecord.go
+++ cmd/inspect/cmd/cleanMapNatRecord.go
@@ -1,1 +1,36 @@
 package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"github.com/spidernet-io/rocktemplate/pkg/ebpf"
+	"os"
+)
+
+var CmdCleanMapNat = &cobra.Command{
+	Use:   "affinity",
+	Short: "clean the ebpf map of natRecord ",
+	Args:  cobra.RangeArgs(0, 0),
+	Run: func(cmd *cobra.Command, args []string) {
+		bpf := ebpf.NewEbpfProgramMananger(nil)
+		if err := bpf.LoadAllEbpfMap(""); err != nil {
+			fmt.Printf("failed to load ebpf Map: %v\n", err)
+			os.Exit(2)
+		}
+		defer bpf.UnloadAllEbpfMap()
+
+		fmt.Printf("\n")
+		fmt.Printf("clean the ebpf map of natRecord:\n")
+		if c, e := bpf.CleanMapNatRecord(); e != nil {
+			fmt.Printf("    failed to clean: %+v\n", e)
+			os.Exit(3)
+		} else {
+			fmt.Printf("    succeeded to clean %d items\n", c)
+		}
+		fmt.Printf("\n")
+	},
+}
+
+func init() {
+	CmdCleanMap.AddCommand(CmdCleanMapNat)
+}
